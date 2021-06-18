e6321f47af4010f4bbb9028f22f73f5a37ac1837
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-18 14:32:19 +0200

    add snack

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
-console.log('ciao');
+const numeriCompresi = (array,min,max) => {
+    const newArrey = array.filter((element,index) => {
+        if (index >= min && index <= max) {           
+            return element ;
+        }
+    });
+    return newArrey;
+}
+console.log(numeriCompresi([1,2,3,4,5,6,7,8,9],3,5));
+
