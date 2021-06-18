9ebd96427d73aa840ca5af3870c451d750bba4c3
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-18 14:14:11 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+console.log('ciao');
